d709d0d517a9749a6616bee7658465b1eeb12414
ricky <Roll08@outlook.com>
2023-04-13 15:50:15 +0800

    2023.4.12

---
+++ myflowable/src/main/java/com/ricky/flowable/myflowable/MyflowableApplication.java
@@ -0,0 +1,13 @@
+package com.ricky.flowable.myflowable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyflowableApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyflowableApplication.class, args);
+    }
+
+}
