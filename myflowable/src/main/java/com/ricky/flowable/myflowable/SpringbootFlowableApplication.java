22c3d6e2f97a7ef2140980f764f59a81c2c60cf7
ricky <Roll08@outlook.com>
2023-04-13 20:02:20 +0800

    2023.4.13

--- myflowable/src/main/java/com/ricky/flowable/myflowable/SpringbootFlowableApplication.java
+++ myflowable/src/main/java/com/ricky/flowable/myflowable/SpringbootFlowableApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MyflowableApplication {
+public class SpringbootFlowableApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MyflowableApplication.class, args);
+        SpringApplication.run(SpringbootFlowableApplication.class, args);
     }
 
 }
